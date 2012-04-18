bedfb0999b7b5575a6fb78e4192d9e6d434e106f
Edward Caissie <edward.caissie@gmail.com>
2012-04-18 12:57:04 -0400

    Add Doc block and improve code quality structure

--- page.php
+++ page.php
@@ -1,21 +1,38 @@
-<?php get_header(); ?>
+<?php
+/**
+ * Page Template
+ *
+ * @package     Shades
+ * @since       1.0
+ *
+ * @link        http://buynowshop.com/themes/shades/
+ * @link        https://github.com/Cais/shades/
+ * @link        http://wordpress.org/extend/themes/shades/
+ *
+ * @internal    REQUIRES WordPress version 3.1.0
+ * @internal    Tested up to WordPress version 3.4
+ *
+ * @author      Edward Caissie <edward.caissie@gmail.com>
+ * @copyright   Copyright (c) 2009-2012, Edward Caissie
+ */
+
+get_header(); ?>
 <div id="maintop"></div>
 <div id="wrapper">
-	<div id="content">
-		<div id="the-loop">
-			<?php if ( have_posts() ) : ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-          <?php get_template_part( 'shades', get_post_format() ); ?>
-          <?php comments_template(); ?>
-				<?php endwhile; ?>
-			<?php else : ?>
-				<h2><?php printf( __( 'Search Results for: %s', 'shades' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?></h2>
-				<p class="center"><?php _e( 'Sorry, but you are looking for something that is not here.', 'shades' ); ?></p>
-				<?php get_search_form(); ?>
-			<?php endif; ?>
-		</div><!-- #the-loop -->
-		<?php get_sidebar(); ?>
-	</div><!-- #content -->
+    <div id="content">
+        <div id="the-loop">
+            <?php
+            if ( have_posts() ) : while ( have_posts() ) : the_post();
+                get_template_part( 'shades', get_post_format() );
+                comments_template();
+            endwhile; else : ?>
+                <h2><?php printf( __( 'Search Results for: %s', 'shades' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?></h2>
+                <p class="center"><?php _e( 'Sorry, but you are looking for something that is not here.', 'shades' ); ?></p>
+                <?php
+                get_search_form();
+            endif; ?>
+        </div><!-- #the-loop -->
+        <?php get_sidebar(); ?>
+    </div><!-- #content -->
 </div><!-- #wrapper -->
-<?php get_footer();?>
-<?php /* Last revised September 10, 2011 v1.7 */ ?>
+<?php get_footer(); ?>
