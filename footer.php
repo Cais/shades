e36afb65ee7828c343dbb3276e8edc5184a5027c
Edward Caissie <edward.caissie@gmail.com>
2012-04-20 14:53:33 -0400

    Documentation updated to note namespace change

--- footer.php
+++ footer.php
@@ -11,6 +11,10 @@
  *
  * @author      Edward Caissie <edward.caissie@gmail.com>
  * @copyright   Copyright (c) 2009-2012, Edward Caissie
+ *
+ * Last revised April 20, 2012
+ * @version     1.8
+ * Changed namespace of *_dynamic_copyright and *_theme_version
  */
 ?>
     <div id="bottom">
