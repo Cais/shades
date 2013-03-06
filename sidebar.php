04413a6a4345ab56fecf00b87864f0f21153664d
Edward Caissie <edward.caissie@gmail.com>
2013-03-06 09:37:20 -0500

    * Refactored code, formatting, and code block termination comments

--- sidebar.php
+++ sidebar.php
@@ -10,41 +10,57 @@
  * @link        http://wordpress.org/extend/themes/shades/
  *
  * @author      Edward Caissie <edward.caissie@gmail.com>
- * @copyright   Copyright (c) 2009-2012, Edward Caissie
+ * @copyright   Copyright (c) 2009-2013, Edward Caissie
+ *
+ * @version     2.1
+ * @date        March 6, 2013
+ * Refactored code, formatting, and code block termination comments
  */ ?>
+
 <div id="sidebar">
+
     <div id="sidebar-top"></div>
+
     <div id="sidebar-content">
+
         <div id="subcolumn">
+
             <div class="widget">
                 <div id="search">
                     <?php get_search_form(); ?>
                 </div><!-- #search -->
             </div>
+
             <ul><li>
-                <?php if ( function_exists( 'dynamic_sidebar' ) && dynamic_sidebar( 1 ) ) : else : ?>
-                    <div class="widget">
+                <?php
+                if ( function_exists( 'dynamic_sidebar' ) && dynamic_sidebar( 1 ) ) : else : ?>
+
+                    <div class="widget calendar">
                         <h2 class="widget-title"><?php _e( 'Calendar', 'shades' ); ?></h2>
                         <div align="center">
                             <?php get_calendar( 0 ); ?>
                         </div>
-                    </div>
-                    <div class="widget">
+                    </div><!-- widget calendar -->
+
+                    <div class="widget bookmarks">
                         <?php wp_list_bookmarks( 'title_li=&title_before=<h2 class="widget-title">&title_after=</h2>&category_before=&category_after=' ); ?>
-                    </div>
-                    <div class="widget">
+                    </div><!-- widget bookmarks -->
+
+                    <div class="widget categories">
                         <h2 class="widget-title"><?php _e( 'Categories', 'shades' ); ?></h2>
                         <ul>
                             <?php wp_list_categories( 'title_li=&show_count=1' ); ?>
                         </ul>
-                    </div>
-                    <div class="widget">
+                    </div><!-- widget categories -->
+
+                    <div class="widget archives">
                         <h2 class="widget-title"><?php _e( 'Archives', 'shades' ); ?></h2>
                         <ul>
                             <?php wp_get_archives( 'type=monthly&show_post_count=1' ); ?>
                         </ul>
-                    </div>
-                    <div class="widget">
+                    </div><!-- widget archives -->
+
+                    <div class="widget meta">
                         <h2 class="widget-title"><?php _e( 'Meta', 'shades' ); ?></h2>
                         <ul>
                             <?php wp_register(); ?>
@@ -52,12 +68,19 @@
                             <li><a href="http://wordpress.org/" title="Powered by WordPress.">WordPress</a></li>
                             <?php wp_meta(); ?>
                         </ul>
-                    </div>
+                    </div><!-- widget meta -->
+
                 <?php
                 endif;
-                if ( function_exists( 'dynamic_sidebar' ) && dynamic_sidebar( 2 ) ) : else : ?><?php endif; ?>
+
+                if ( function_exists( 'dynamic_sidebar' ) && dynamic_sidebar( 2 ) ) : else : endif; ?>
+
             </li></ul>
+
         </div><!-- #subcolumn -->
+
     </div><!--#sidebar-content -->
+
     <div id="sidebar-bottom"></div>
+
 </div><!-- #sidebar -->
