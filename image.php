657bdc83fc3716f09d5505cf5780c070434bfe0e
Edward Caissie <edward.caissie@gmail.com>
2013-03-06 09:11:58 -0500

    * Refactored code formatting and code block termination comments

--- image.php
+++ image.php
@@ -11,12 +11,17 @@
  * @link        http://wordpress.org/extend/themes/shades/
  *
  * @author      Edward Caissie <edward.caissie@gmail.com>
- * @copyright   Copyright (c) 2009-2012, Edward Caissie
+ * @copyright   Copyright (c) 2009-2013, Edward Caissie
  *
  * @version     2.0
  * @date        December 7, 2012
  * Refactored conditional checks for "author credits with copyright details"
+ *
+ * @version     2.1
+ * @date        March 6, 2013
+ * Refactored code formatting and code block termination comments
  */
+
 get_header(); ?>
 <div id="maintop"></div>
 <div id="wrapper">
@@ -49,18 +54,18 @@ get_header(); ?>
                         $shades_image_meta['width'] . 'px',
                         $shades_image_meta['height'] . 'px' )
                     . '</div>';
-            }
+            } /** End if - image meta - width / height */
 
             /** Author Credit with Copyright details */
             if ( $shades_image_meta['image_meta']['credit'] ) {
                 echo '<br />' . sprintf( __( 'Credit: %1$s', 'shades' ), $shades_image_meta['image_meta']['credit'] );
                 if ( $shades_image_meta['image_meta']['copyright'] ) {
                     echo ' ';
-                }
-            }
+                } /** End if - image meta - copyright */
+            } /** End if - image meta - credit */
             if ( $shades_image_meta['image_meta']['copyright'] ) {
                 printf( '&copy; %1$s %2$s', get_the_time( 'Y' ), $shades_image_meta['image_meta']['copyright'] );
-            }
+            } /** End if - image meta - copyright */
 
             /** Creation timestamp in end-user settings format */
             if ( $shades_image_meta['image_meta']['created_timestamp'] ) {
@@ -68,12 +73,12 @@ get_header(); ?>
                     . sprintf( __( 'Created (timestamp): %1$s', 'shades' ),
                         get_the_time( get_option( 'date_format' ), $shades_image_meta['image_meta']['created_timestamp'] )
                     );
-            }
+            } /** End if - image meta - timestamp */
 
             /** Camera details */
             if ( $shades_image_meta['image_meta']['camera'] ) {
                 echo '<br />' . sprintf( __( 'Camera: %1$s', 'shades' ), $shades_image_meta['image_meta']['camera'] );
-            }
+            } /** End if - image meta - camera */
 
             /** Shutter speed */
             if ( $shades_image_meta['image_meta']['shutter_speed'] ) {
@@ -91,33 +96,33 @@ get_header(); ?>
                     }
                 } else {
                     echo $shades_image_meta['image_meta']['shutter_speed'] . ' ' . __( 'sec', 'shades' );
-                }
-            }
+                } /** End if image meta - shutter speed */
+            } /** End if - image meta - shutter speed */
 
             /** Aperture Setting */
             if ( $shades_image_meta['image_meta']['aperture'] ) {
                 echo '<br />' . sprintf( __( 'Aperture (F-stop): %1$s', 'shades' ), $shades_image_meta['image_meta']['aperture'] );
-            }
+            } /** End if - image meta - aperture */
 
             /** Image caption from EXIF details */
             if ( $shades_image_meta['image_meta']['caption'] ) {
                 echo '<br />' . sprintf( __( 'Caption: %1$s', 'shades' ), $shades_image_meta['image_meta']['caption'] );
-            }
+            } /** End if - image meta - caption */
 
             /** Focal Length - "mm" is used as the short-form for millimeters */
             if ( $shades_image_meta['image_meta']['focal_length'] ) {
                 echo '<br />' . sprintf( __( 'Focal Length: %1$s', 'shades' ), $shades_image_meta['image_meta']['focal_length'] ) . 'mm';
-            }
+            } /** End if - image meta - focal length */
 
             /** ISO Speed */
             if ( $shades_image_meta['image_meta']['iso'] ) {
                 echo '<br />' . sprintf( __( 'Speed: ISO %1$s', 'shades' ), $shades_image_meta['image_meta']['iso'] );
-            }
+            } /** End if - image meta - ISO */
 
             /** Title from EXIF details */
             if ( $shades_image_meta['image_meta']['title'] ) {
                 echo '<br />' . sprintf( __( 'Title: %1$s', 'shades' ), $shades_image_meta['image_meta']['title'] );
-            } ?>
+            } /** End if - image meta - title */ ?>
 
         </div><!-- #the-loop -->
         <?php get_sidebar(); ?>
